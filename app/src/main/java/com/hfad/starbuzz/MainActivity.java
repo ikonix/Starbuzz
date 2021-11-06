984c0852b16104c7ada07fb6b080fe4e032fc902
Alexander <ikonix1304@gmail.com>
2021-11-06 20:44:00 +0100

    DrinkCategoryActivity

--- app/src/main/java/com/hfad/starbuzz/MainActivity.java
+++ app/src/main/java/com/hfad/starbuzz/MainActivity.java
@@ -2,7 +2,11 @@ package com.hfad.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                if(position == 0) {
+                    Intent intent = new Intent(MainActivity.this, DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+
+        ListView listView = findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
